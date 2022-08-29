d9bb56b88df9cb30dbab6bd99cbde5cdd5acd058
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-08-29 23:03:39 +0700

    Add `Dockerfile-1.18_alpine-wkhtmltopdf`

---
+++ golang/install_wkhtmltopdf_alpine.sh
@@ -0,0 +1,22 @@
+set -ex; \
+apk add --no-cache \
+    libstdc++ \
+    libx11 \
+    libxrender \
+    libxext \
+    libssl1.1 \
+    ca-certificates \
+    fontconfig \
+    freetype \
+    ttf-dejavu \
+    ttf-droid \
+    ttf-freefont \
+    ttf-liberation; \
+apk add --no-cache --virtual .build-deps \
+    msttcorefonts-installer; \
+update-ms-fonts; \
+fc-cache -f; \
+rm -rf /tmp/*; \
+apk del --no-cache --no-network .build-deps; \
+rm -f install_wkhtmltopdf_alpine.sh
+
