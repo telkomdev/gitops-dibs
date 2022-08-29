457b44561a6acc1f7c94f371b43fec7464f097ca
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-08-29 21:24:03 +0700

    Add `install_gitleaks_alpine.sh`

---
+++ gitleaks/install_gitleaks_alpine.sh
@@ -0,0 +1,29 @@
+set -ex; \
+adduser -D gitleaks; \
+apk add --no-cache \
+    curl \
+    ca-certificates \
+    tar; \
+apk add --no-cache \
+    bash \
+    git \
+    openssh-client; \
+alpineArch="$(apk --print-arch)"; \
+case "${alpineArch##*-}" in \
+  x86_64) \
+    SOURCE="https://github.com/zricethezav/gitleaks/releases/download/v${GITLEAKS_VERSION}/gitleaks_${GITLEAKS_VERSION}_linux_x64.tar.gz"; \
+    CHECKSUM="6f7c60f4b8194c0d8d860dd7af65361d4dc52b2df1aafd03287ea3446a75e9cd"; \
+    ;; \
+  *) \
+    ;; \
+esac; \
+curl -Lo gitleaks.tar.gz "${SOURCE}"; \
+echo "${CHECKSUM}  gitleaks.tar.gz" | sha256sum -c - || exit 1; \
+tar -xzvpf gitleaks.tar.gz; \
+cp -avp gitleaks /usr/local/bin/gitleaks; \
+rm -rf *; \
+gitleaks version || exit 1; \
+apk del --no-cache --no-network \
+    tar; \
+rm -f install_gitleaks_alpine.sh
+
