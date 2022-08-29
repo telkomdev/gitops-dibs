c22031667f3d904adfe8034e92f1597616935991
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-08-29 16:52:00 +0700

    Add hardlink `install_oci_debian.sh`

---
+++ nodejs/install_oci_debian.sh
@@ -0,0 +1,24 @@
+set -ex; \
+apt-get update -y; \
+apt-get install -y --no-install-recommends \
+    libaio1; \
+apt-get install -y --no-install-recommends \
+    ca-certificates \
+    curl \
+    unzip; \
+curl -Lo instantclient.zip "https://download.oracle.com/otn_software/linux/instantclient/217000/instantclient-basiclite-linux.x64-${INSTANT_CLIENT_VERSION}.0.0dbru.zip"; \
+sha256_oci="8a745ad7f4290ff8f7bd1d9436f6afdf07644e390b5d6acc3dc50978687795cb"; \
+echo "${sha256_oci}  instantclient.zip" | sha256sum -c - || exit 1; \
+unzip instantclient.zip; \
+rm -f instantclient.zip; \
+mkdir -p ${ORACLE_BASE}; \
+mv instantclient_*/* ${ORACLE_BASE}/; \
+rm -rf instantclient_*; \
+echo "${ORACLE_BASE}" > /etc/ld.so.conf.d/oracle-instantclient.conf; \
+ldconfig; \
+apt-get remove -y --purge \
+    ca-certificates \
+    curl \
+    unzip; \
+rm -f install_oci_debian.sh
+
