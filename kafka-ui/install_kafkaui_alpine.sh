9d8ebdae7a918d5097202e8df7614f852d03a928
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-08-29 21:02:50 +0700

    Add `install_kafkaui_alpine.sh`

---
+++ kafka-ui/install_kafkaui_alpine.sh
@@ -0,0 +1,12 @@
+set -ex; \
+apk add --no-cache \
+    openjdk13-jre; \
+apk add --no-cache \
+    curl; \
+curl -Lo kafka-ui-api.jar "https://github.com/provectus/kafka-ui/releases/download/v${KAFKA_UI_VERSION}/kafka-ui-api-v${KAFKA_UI_VERSION}.jar"; \
+sha256="cedd15fab7de6d3f4616b69b5e9f3e456a57e927bc7809e2e8f6bc500670ac68"; \
+echo "${sha256}  kafka-ui-api.jar" | sha256sum -c - || exit 1; \
+apk del --purge \
+    curl; \
+rm install_kafkaui_alpine.sh
+
