37587d32fb58ac1535df2066b7eceaaf7f81fdd0
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-08-30 09:18:01 +0700

    Update kafka-ui debian builds

--- kafka-ui/install_kafkaui_debian.sh
+++ kafka-ui/install_kafkaui_debian.sh
@@ -5,7 +5,11 @@ apt-get install -y --no-install-recommends \
 apt-get install -y --no-install-recommends \
   curl; \
 curl -Lo kafka-ui-api.jar "https://github.com/provectus/kafka-ui/releases/download/v${KAFKA_UI_VERSION}/kafka-ui-api-v${KAFKA_UI_VERSION}.jar"; \
-sha256="cedd15fab7de6d3f4616b69b5e9f3e456a57e927bc7809e2e8f6bc500670ac68"; \
+case $(echo ${KAFKA_UI_VERSION} | awk -F[\.] '{print $1"."$2}') in \
+  '0.4') \
+    sha256="cedd15fab7de6d3f4616b69b5e9f3e456a57e927bc7809e2e8f6bc500670ac68"; \
+    ;; \
+esac; \
 echo "${sha256}  kafka-ui-api.jar" | sha256sum -c - || exit 1; \
 apt-get remove -y --purge \
     curl; \
