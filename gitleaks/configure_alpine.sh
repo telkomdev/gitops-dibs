d55187095eacc833783cbb0d4dcc3a1854ebf6b0
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-09-28 12:07:39 +0700

    Update configure_alpine.sh

--- gitleaks/configure_alpine.sh
+++ gitleaks/configure_alpine.sh
@@ -1,6 +1,11 @@
 set -ex; \
 apk add --no-cache tzdata; \
-rm -rf /var/cache/apk/*; \
-rm -rf /tmp/*; \
-rm -f configure_alpine.sh
+cd /usr/src/app; \
+rm -rf \
+    /var/cache/apk/* \
+    /tmp/* \
+    configure_alpine.sh \
+    install_glibc_alpine.sh \
+    install_oci_alpine.sh \
+    install_wkhtmltopdf_alpine.sh
 
