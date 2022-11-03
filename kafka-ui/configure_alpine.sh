4da91afaa51cdacc3512d0f7c860706410d4001e
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-11-03 13:53:16 +0700

    Update configure scripts

--- kafka-ui/configure_alpine.sh
+++ kafka-ui/configure_alpine.sh
@@ -4,8 +4,5 @@ cd /usr/src/app; \
 rm -rf \
     /var/cache/apk/* \
     /tmp/* \
-    configure_alpine.sh \
-    install_glibc_alpine.sh \
-    install_oci_alpine.sh \
-    install_wkhtmltopdf_alpine.sh
+    ./*
 
--- kafka-ui/configure_debian.sh
+++ kafka-ui/configure_debian.sh
@@ -4,8 +4,9 @@ apt-get install -y --no-install-recommends \
 apt-get autoremove -y --purge; \
 apt-get autoclean -y; \
 apt-get clean -y; \
-rm -rf /var/cache/apt/*; \
-rm -rf /var/lib/apt/lists/*; \
-rm -rf /tmp/*; \
-rm -f configure_debian.sh
+cd /usr/src/app; \
+rm -rf /var/cache/apt/* \
+    /var/lib/apt/lists/* \
+    /tmp/* \
+    ./*
 
