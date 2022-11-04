3e4c4f9e0776dd7b651d3094b9d6619e495ebe75
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-11-04 11:12:03 +0700

    Add missing configure_alpine.sh on nginx builds

---
+++ nginx/configure_alpine.sh
@@ -0,0 +1,13 @@
+set -ex; \
+apk add --no-cache \
+    bash \
+    tzdata; \
+cd /usr/src/app; \
+rm -rf \
+    /var/cache/apk/* \
+    /tmp/* \
+    ./*; \
+adduser -S -u 1000 -G root user; \
+chmod -R 755 /usr/src/app; \
+chown -R user:root /usr/src/app
+
