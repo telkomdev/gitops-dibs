84682a0ddd91d8870b1bdce58e8d984ee045d6e2
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-11-05 11:58:24 +0700

    Add glibc 2.34 option

--- rust/install_glibc_alpine.sh
+++ rust/install_glibc_alpine.sh
@@ -20,9 +20,18 @@ wget \
     "${ALPINE_GLIBC_BASE_URL}/${GLIBC_VERSION}/${ALPINE_GLIBC_BASE_PACKAGE_FILENAME}" \
     "${ALPINE_GLIBC_BASE_URL}/${GLIBC_VERSION}/${ALPINE_GLIBC_BIN_PACKAGE_FILENAME}" \
     "${ALPINE_GLIBC_BASE_URL}/${GLIBC_VERSION}/${ALPINE_GLIBC_I18N_PACKAGE_FILENAME}"; \
-sha256_glibc_base="02fe2d91f53eab93c64d74485b80db575cfb4de40bc0d12bf55839fbe16cb041"; \
-sha256_glibc_bin="720b870b51f64324e318370eda080b5ee99332c36b80be975968136e06c1af93"; \
-sha256_glibc_i18n="171062071e7db01e4daefc2acdb798da5ed410df1078226d755c08c46c124063"; \
+case $(echo "${GLIBC_VERSION}" | '{print $1"."$2}') in \
+  '2.34') \
+    sha256_glibc_base="3ef4a8d71777b3ccdd540e18862d688e32aa1c7bc5a1c0170271a43d0e736486"; \
+    sha256_glibc_bin="cc631135f481644ce74b28f6e37ceea848de06389d669182a012a9ca1ac5a95a"; \
+    sha256_glibc_i18n="265e3cd7e958be00e3aa16b7713e958d7eaa43290d0aab89ae6fc80267e18dd4"; \
+    ;; \
+  '2.35') \
+    sha256_glibc_base="02fe2d91f53eab93c64d74485b80db575cfb4de40bc0d12bf55839fbe16cb041"; \
+    sha256_glibc_bin="720b870b51f64324e318370eda080b5ee99332c36b80be975968136e06c1af93"; \
+    sha256_glibc_i18n="171062071e7db01e4daefc2acdb798da5ed410df1078226d755c08c46c124063"; \
+    ;; \
+esac; \
 echo "${sha256_glibc_base}  ${ALPINE_GLIBC_BASE_PACKAGE_FILENAME}" | sha256sum -c - || exit 1; \
 echo "${sha256_glibc_bin}  ${ALPINE_GLIBC_BIN_PACKAGE_FILENAME}" | sha256sum -c - || exit 1; \
 echo "${sha256_glibc_i18n}  ${ALPINE_GLIBC_I18N_PACKAGE_FILENAME}" | sha256sum -c - || exit 1; \
