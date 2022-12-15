9c572be3202bae9fa153f003f2120b19a3dfc0e2
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-12-15 08:52:07 +0700

    Add gitleaks build v8.15

--- gitleaks/install_gitleaks_alpine.sh
+++ gitleaks/install_gitleaks_alpine.sh
@@ -12,7 +12,14 @@ alpineArch="$(apk --print-arch)"; \
 case "${alpineArch##*-}" in \
   x86_64) \
     SOURCE="https://github.com/zricethezav/gitleaks/releases/download/v${GITLEAKS_VERSION}/gitleaks_${GITLEAKS_VERSION}_linux_x64.tar.gz"; \
-    CHECKSUM="6f7c60f4b8194c0d8d860dd7af65361d4dc52b2df1aafd03287ea3446a75e9cd"; \
+    case $(echo ${GITLEAKS_VERSION} | awk -F[\.] '{print $1"."$2}') in \
+      '8.11') \
+        CHECKSUM="6f7c60f4b8194c0d8d860dd7af65361d4dc52b2df1aafd03287ea3446a75e9cd"; \
+        ;; \
+      '8.15') \
+        CHECKSUM="43e007867e49376fac8953fc1be3ba540b4cefacec40ee9d4b465a9fb9ae3b8d"; \
+        ;; \
+    esac; \
     ;; \
   *) \
     ;; \
